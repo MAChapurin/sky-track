83e73f560c636dd73a2a473a9edd89eb65d9051a
MAChapurin <machapurin@icloud.com>
2025-07-05 21:58:57 -0700

    add navlink for desktop

--- src/features/nav-desktop/ui/nav-desktop.tsx
+++ src/features/nav-desktop/ui/nav-desktop.tsx
@@ -1,34 +1,50 @@
 import { PATHNAMES } from '@/shared/config'
 import { Icon } from '@/shared/ui/icon'
 import { cn } from '@/shared/utils'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router'
 
 export const NavDesktop = () => {
   return (
     <nav className="hidden lg:grid grid-cols-3 gap-1">
-      <Link
-        className={cn(
-          'animate-fade-left flex items-center bg-primary dark:bg-primary-dark transition-colors gap-2 p-2 rounded-l-2xl text-xl',
-          {
-            ['bg-accent dark:bg-accent-dark']: true
-          }
-        )}
+      <NavLink
+        className={({ isActive }) =>
+          cn(
+            'animate-fade-left flex items-center bg-primary dark:bg-primary-dark transition-colors gap-2 p-2 rounded-l-2xl text-xl',
+            {
+              ['bg-accent dark:bg-accent-dark']: isActive
+            }
+          )
+        }
         to={PATHNAMES.HOME}
       >
         <Icon name="home" /> Main
-      </Link>
-      <Link
-        className="animate-fade-left flex items-center bg-primary dark:bg-primary-dark transition-colors gap-2 p-2 text-xl animate-delay-100"
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          cn(
+            'animate-fade-left flex items-center bg-primary dark:bg-primary-dark transition-colors gap-2 p-2 text-xl animate-delay-100',
+            {
+              ['bg-accent dark:bg-accent-dark']: isActive
+            }
+          )
+        }
         to={PATHNAMES.RACES}
       >
         <Icon name="columns" /> Races
-      </Link>
-      <Link
+      </NavLink>
+      <NavLink
         to={PATHNAMES.ABOUT}
-        className="animate-fade-left flex items-center bg-primary dark:bg-primary-dark transition-colors gap-2 p-2 rounded-r-2xl text-xl animate-delay-200"
+        className={({ isActive }) =>
+          cn(
+            'animate-fade-left flex items-center bg-primary dark:bg-primary-dark transition-colors gap-2 p-2 rounded-r-2xl text-xl animate-delay-200',
+            {
+              ['bg-accent dark:bg-accent-dark']: isActive
+            }
+          )
+        }
       >
         <Icon name="logo" className="h-4 w-4" /> About
-      </Link>
+      </NavLink>
     </nav>
   )
 }
